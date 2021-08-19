6cafd3dfb00ef53d20a8e6c962d0e200a952a97a
DucNghia28 <89195636+DucNghia28@users.noreply.github.com>
2021-08-19 18:16:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 const CONFIG = {
     introTitle: 'Em Yêuu à !',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Let me tie your shoes, cause I dont want you falling for anyone else
+    Let’s commit the perfect crime: I’ll steal you’re heart, and you’ll steal mine`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
