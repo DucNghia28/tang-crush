1d8eea129890b631258688e3121a3615a2765825
DucNghia28 <89195636+DucNghia28@users.noreply.github.com>
2021-08-19 19:32:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
-    introTitle: 'Heyy Baeee, these are some silly things from the bottom of mah heart ;) ',
+    introTitle: 'Heyy Baeee, these are some silly things from the bottom of mah heart.. ',
     introDesc: `Let me tie your shoes, cause I dont want you falling for anyone else.😘😘 `,
-    btnIntro: 'hihi <3',
+    btnIntro: 'hehe <3',
     title: 'People are catching Coronavirus but the only thing I’m catching is feelings for you 😙',
     desc: ' Wil you be mine ? ',
     btnYes: 'You are mine, and im yours <3',
@@ -10,7 +10,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Vậy là e chính thức là của anh rùi nhé :P. Hết dịch đi chơi với anh nha bé :** ',
-    btnAccept: 'Okiiiii lun ạ <3',
+    messDesc: 'Vậy là chúng mình chính thức là của nhau rùi nhaaaa 😘 ',
+    btnAccept: 'Vânggg ạ <3',
     messLink: 'https://www.facebook.com/messages/t/100003718135325' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
