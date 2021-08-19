4214a895e95630048e93075caae717e68ab40d12
DucNghia28 <89195636+DucNghia28@users.noreply.github.com>
2021-08-19 18:29:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     introTitle: 'Em Yêuu à !',
-    introDesc: `Let me tie your shoes, cause I dont want you falling for anyone else
-    Let’s commit the perfect crime: I’ll steal you’re heart, and you’ll steal mine`,
-    btnIntro: 'hihi',
+    introDesc: `Let me tie your shoes, cause I dont want you falling for anyone else.😘😘
+    People are catching Coronavirus but the only thing I’m catching is feelings for you 😙 `,
+    btnIntro: 'hihi <3',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Thực ra em thích anh lắm <3',
+    btnNo: 'Không, Em cần thêm thời gian :(',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Hết dịch đi chơi với anh nha bé ^^ ',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Vậy là e chính thức là của anh rùi nhé :P. Hết dịch đi chơi với anh nha bé :** ',
+    btnAccept: 'Okiiiii lun ạ <3',
     messLink: 'https://www.facebook.com/messages/t/100003718135325' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
